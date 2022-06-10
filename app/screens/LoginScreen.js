32be1711fcaebfdfd6ec3993cea47ebc77424706
khalfan <khalfan34747@gmail.com>
2022-06-10 17:51:28 +0400

    added some files

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,76 +1,65 @@
-import React from 'react';
-import { Image, StyleSheet } from 'react-native';
-import * as Yup from 'yup';
-import {Formik} from 'formik';
+import React from "react";
+import { Image, StyleSheet } from "react-native";
+import * as Yup from "yup";
 
+import Screen from "../components/Screen";
+import { AppForm, AppFormField, SubmitButton } from "../components/Forms";
 
-import AppButton from '../components/AppButton';
-import AppTextInput from '../components/AppTextInput';
-import ErrorMessage from '../components/ErrorMessage';
-import Screen from '../components/Screen';
-
+// To Check if the Inputs validated or not (Email , Password)
 const validationSchema = Yup.object().shape({
-    email : Yup.string().required().email().label('Email'),
-    password : Yup.string().required().min(4).label('Password')
+  email: Yup.string().required().email().label("Email"),
+  password: Yup.string().required().min(4).label("Password"),
 });
 
 function LoginScreen(props) {
-    return (
-        <Screen style={styles.container}>
-            <Image style = {styles.logo} source={require('../assets/logo-red.png')}/>
-            
-            <Formik
-                initialValues={{email :'' , password:''}}
-                onSubmit={values => console.log(values)}
-                validationSchema={validationSchema}
-                >
-                    {({ handleChange , handleSubmit , errors , setFieldTouched , touched}) => (
-                        <>
-                        <AppTextInput
-                            autoCapitalize='none'
-                            autoCorrext={false}
-                            icon='email'
-                            keyboardType='email-address'
-                            onBlur={() => setFieldTouched('email')}
-                            onChangeText={handleChange('email')}
-                            placeholder='Email'
-                            textContentType='emailAddress'
-                        />
+  return (
+    <Screen style={styles.container}>
+      <Image style={styles.logo} source={require("../assets/logo-red.png")} />
 
-                        <ErrorMessage error={errors.email} visible={touched.email}/>
-                        <AppTextInput
-                            autoCapitalize ='none'
-                            autoCorrext={false}
-                            icon='lock'
-                            onBlur={() => setFieldTouched('password')}
-                            onChangeText={handleChange('password')}
-                            placeholder='Password'
-                            secureTextEntry
-                            textContentType='password'
-                        />
-                        <ErrorMessage error={errors.password} visible={touched.password}/>
-                        <AppButton title='Login' onPress={handleSubmit}/>
-                        </>
-                    ) }
-                </Formik>
+      <AppForm
+        initialValues={{ email: "", password: "" }}
+        onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}
+      >
+        <AppFormField
+          autoCapitalize="none"
+          autoCorrect={false}
+          icon="email"
+          keyboardType="email-address"
+          name="email"
+          placeholder="Email"
+          textContentType="emailAddress"
+        />
 
-            
-        </Screen>
-    );
+        {/* <ErrorMessage error={errors.email} visible={touched.email}/> */}
+        <AppFormField
+          autoCapitalize="none"
+          autoCorrect={false}
+          icon="lock"
+          name="password"
+          placeholder="Password"
+          secureTextEntry
+          textContentType="password"
+        />
+        {/* <ErrorMessage error={errors.password} visible={touched.password}/> */}
+        <SubmitButton title="Login" />
+      </AppForm>
+    </Screen>
+  );
 }
 
 export default LoginScreen;
 
 const styles = StyleSheet.create({
-    container:{
-        padding : 10,
-    },
+  container: {
+    padding: 10,
+  },
 
-    logo:{
-        width: 80,
-        height: 80,
-        alignSelf:'center',
-        marginTop: 50,
-        marginBottom: 20
-    },
-})
+  logo: {
+    width: 80,
+    height: 80,
+    alignSelf: "center",
+    marginTop: 50,
+    marginBottom: 20,
+  },
+});
